3c2398c644653459544bc56643bf337b4365ed9f
maliq273 <maliq273@gmail.com>
2026-08-06 15:15:05 +0200

    Update MatterSource.js

--- app/domain/enums/MatterSource.js
+++ app/domain/enums/MatterSource.js
@@ -1,1 +1,19 @@
+const MatterSource = Object.freeze({
 
+    WEBSITE: "WEBSITE",
+
+    WALK_IN: "WALK_IN",
+
+    WHATSAPP: "WHATSAPP",
+
+    PHONE: "PHONE",
+
+    EMAIL: "EMAIL",
+
+    REFERRAL: "REFERRAL",
+
+    SOCIAL_MEDIA: "SOCIAL_MEDIA"
+
+});
+
+export default MatterSource;
