5093ba4ee92ccdcf19be4aa01d2bba4f2e32406f
maliq273 <maliq273@gmail.com>
2026-08-06 15:17:31 +0200

    Define PaymentStatus enum with various states

--- app/domain/enums/PaymentStatus.js
+++ app/domain/enums/PaymentStatus.js
@@ -1,1 +1,15 @@
+const PaymentStatus = Object.freeze({
 
+    UNPAID: "UNPAID",
+
+    PARTIAL: "PARTIAL",
+
+    PAID: "PAID",
+
+    REFUNDED: "REFUNDED",
+
+    OVERDUE: "OVERDUE"
+
+});
+
+export default PaymentStatus;
