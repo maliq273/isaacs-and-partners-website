b6f36b865ff595d5e33561c3bcc8b424ef131115
maliq273 <maliq273@gmail.com>
2026-08-06 15:18:07 +0200

    Update InvoiceStatus.js

--- app/domain/enums/InvoiceStatus.js
+++ app/domain/enums/InvoiceStatus.js
@@ -1,1 +1,15 @@
+const InvoiceStatus = Object.freeze({
 
+    DRAFT: "DRAFT",
+
+    ISSUED: "ISSUED",
+
+    PARTIAL: "PARTIAL",
+
+    PAID: "PAID",
+
+    CANCELLED: "CANCELLED"
+
+});
+
+export default InvoiceStatus;
