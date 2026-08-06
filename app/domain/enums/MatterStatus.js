1044bad2a409c7ecf6f89ff2f75a83a9c0856ce3
maliq273 <maliq273@gmail.com>
2026-08-06 15:12:08 +0200

    Update MatterStatus.js

--- app/domain/enums/MatterStatus.js
+++ app/domain/enums/MatterStatus.js
@@ -1,1 +1,26 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS
+ * Matter Status
+ * ============================================================
+ */
 
+const MatterStatus = Object.freeze({
+
+    NEW: "NEW",
+
+    OPEN: "OPEN",
+
+    ACTIVE: "ACTIVE",
+
+    ON_HOLD: "ON_HOLD",
+
+    CLOSED: "CLOSED",
+
+    CANCELLED: "CANCELLED",
+
+    ARCHIVED: "ARCHIVED"
+
+});
+
+export default MatterStatus;
