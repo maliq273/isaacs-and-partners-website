15c0d0ff5dc1dd9ad62dd6dac4f92f058ca85b53
maliq273 <maliq273@gmail.com>
2026-08-06 15:14:02 +0200

    Update MatterType.js

--- app/domain/enums/MatterType.js
+++ app/domain/enums/MatterType.js
@@ -1,1 +1,19 @@
+const MatterType = Object.freeze({
 
+    IMMIGRATION: "IMMIGRATION",
+
+    HR: "HR",
+
+    LEGAL: "LEGAL",
+
+    CCMA: "CCMA",
+
+    PAYROLL: "PAYROLL",
+
+    BUSINESS: "BUSINESS",
+
+    NOTARY: "NOTARY"
+
+});
+
+export default MatterType;
