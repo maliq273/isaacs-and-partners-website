a0c7436a4ed39ab33f3976b37ab7fb7955e4657e
maliq273 <maliq273@gmail.com>
2026-08-06 14:56:59 +0200

    Create Entity class for domain objects
    Added Entity class as a base for identifiable domain objects with methods for equality, cloning, updating, and checking active status.

--- app/domain/Entity.js
+++ app/domain/Entity.js
@@ -1,1 +1,80 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Entity
+ * ------------------------------------------------------------
+ * Base class for all identifiable domain objects.
+ * ============================================================
+ */
 
+import BaseModel from "./BaseModel.js";
+
+export default class Entity extends BaseModel {
+
+    constructor(id = null) {
+
+        super();
+
+        if (id) {
+
+            this.id = id;
+
+        } else {
+
+            this.ensureId();
+
+        }
+
+    }
+
+    /**
+     * Compare entities by ID
+     */
+
+    equals(entity) {
+
+        if (!entity) {
+
+            return false;
+
+        }
+
+        return this.id === entity.id;
+
+    }
+
+    /**
+     * Clone this entity
+     */
+
+    clone() {
+
+        return new this.constructor().fromJSON(this.toJSON());
+
+    }
+
+    /**
+     * Mark entity as modified
+     */
+
+    markUpdated(updatedBy = null) {
+
+        this.updatedBy = updatedBy;
+
+        this.touch();
+
+        return this;
+
+    }
+
+    /**
+     * Returns true if entity is active
+     */
+
+    isActive() {
+
+        return this.active && !this.deleted;
+
+    }
+
+}
