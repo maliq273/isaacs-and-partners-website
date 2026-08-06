bef9d5e4241563b934d30bb6f334a19d16eac828
maliq273 <maliq273@gmail.com>
2026-08-06 15:16:08 +0200

    Update MatterOutcome.js

--- app/domain/enums/MatterOutcome.js
+++ app/domain/enums/MatterOutcome.js
@@ -1,1 +1,15 @@
+const MatterOutcome = Object.freeze({
 
+    PENDING: "PENDING",
+
+    APPROVED: "APPROVED",
+
+    REJECTED: "REJECTED",
+
+    WITHDRAWN: "WITHDRAWN",
+
+    APPEALED: "APPEALED"
+
+});
+
+export default MatterOutcome;
