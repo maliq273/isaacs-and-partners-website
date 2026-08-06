c43dad3e21189db93f9dca14b6d231003fc89d2d
maliq273 <maliq273@gmail.com>
2026-08-06 15:39:21 +0200

    Update TimelineEntry.js

--- app/models/TimelineEntry.js
+++ app/models/TimelineEntry.js
@@ -1,1 +1,40 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Timeline Entry
+ * ============================================================
+ */
 
+import Record from "./base/Record.js";
+
+export default class TimelineEntry extends Record {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.title = data.title ?? "";
+
+        this.description = data.description ?? "";
+
+        this.type = data.type ?? "GENERAL";
+
+        this.icon = data.icon ?? "";
+
+        this.visibleToClient = data.visibleToClient ?? true;
+
+    }
+
+    validate() {
+
+        if (!this.title) {
+
+            throw new Error("Timeline title is required.");
+
+        }
+
+        return true;
+
+    }
+
+}
