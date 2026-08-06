ae3bccd5a3bb65670d09e07078f926cddc9d600e
maliq273 <maliq273@gmail.com>
2026-08-06 15:36:33 +0200

    Update Record.js

--- app/models/base/Record.js
+++ app/models/base/Record.js
@@ -1,1 +1,54 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Record
+ * ------------------------------------------------------------
+ * Base class for lightweight records that belong to a Matter.
+ * ============================================================
+ */
 
+import Entity from "../../domain/Entity.js";
+
+export default class Record extends Entity {
+
+    constructor(data = {}) {
+
+        super(data.id);
+
+        this.matterId = data.matterId ?? null;
+
+        this.createdBy = data.createdBy ?? null;
+
+        this.updatedBy = data.updatedBy ?? null;
+
+        this.metadata = data.metadata ?? {};
+
+    }
+
+    setMatter(matterId) {
+
+        this.matterId = matterId;
+
+        this.touch();
+
+        return this;
+
+    }
+
+    setMetadata(metadata = {}) {
+
+        this.metadata = {
+
+            ...this.metadata,
+
+            ...metadata
+
+        };
+
+        this.touch();
+
+        return this;
+
+    }
+
+}
