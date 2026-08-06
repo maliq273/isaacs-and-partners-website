8797e546aaa042f8bd34e27597e08f1480c5acca
maliq273 <maliq273@gmail.com>
2026-08-06 15:42:39 +0200

    Update Communication.js

--- app/models/Communication.js
+++ app/models/Communication.js
@@ -1,1 +1,44 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Communication
+ * ============================================================
+ */
 
+import Record from "./base/Record.js";
+
+export default class Communication extends Record {
+
+    constructor(data = {}) {
+
+        super(data);
+
+        this.type = data.type ?? "EMAIL";
+
+        this.direction = data.direction ?? "OUTBOUND";
+
+        this.subject = data.subject ?? "";
+
+        this.message = data.message ?? "";
+
+        this.sender = data.sender ?? "";
+
+        this.recipient = data.recipient ?? "";
+
+        this.sentAt = data.sentAt ?? new Date().toISOString();
+
+        this.read = false;
+
+    }
+
+    markRead() {
+
+        this.read = true;
+
+        this.touch();
+
+        return this;
+
+    }
+
+}
