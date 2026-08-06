1a701e8fd27a495a207757bb2833391a964456ba
maliq273 <maliq273@gmail.com>
2026-08-06 15:14:29 +0200

    Update MatterDepartment.js

--- app/domain/enums/MatterDepartment.js
+++ app/domain/enums/MatterDepartment.js
@@ -1,1 +1,17 @@
+const MatterDepartment = Object.freeze({
 
+    IMMIGRATION: "IMMIGRATION",
+
+    HR_IR: "HR_IR",
+
+    LEGAL: "LEGAL",
+
+    BUSINESS_COMPLIANCE: "BUSINESS_COMPLIANCE",
+
+    NOTARY: "NOTARY",
+
+    PAYROLL: "PAYROLL"
+
+});
+
+export default MatterDepartment;
