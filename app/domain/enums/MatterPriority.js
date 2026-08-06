5a5831d019c91176784ef684ecb4f362511582f6
maliq273 <maliq273@gmail.com>
2026-08-06 15:12:54 +0200

    Define MatterPriority enum with various levels

--- app/domain/enums/MatterPriority.js
+++ app/domain/enums/MatterPriority.js
@@ -1,1 +1,17 @@
+const MatterPriority = Object.freeze({
 
+    LOW: "LOW",
+
+    NORMAL: "NORMAL",
+
+    MEDIUM: "MEDIUM",
+
+    HIGH: "HIGH",
+
+    URGENT: "URGENT",
+
+    CRITICAL: "CRITICAL"
+
+});
+
+export default MatterPriority;
