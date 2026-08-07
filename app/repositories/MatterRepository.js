9fb9200b083e2b9c0767bca61b0d6794b4f620d4
maliq273 <maliq273@gmail.com>
2026-08-07 19:23:21 +0200

    Update MatterRepository.js

--- app/repositories/MatterRepository.js
+++ app/repositories/MatterRepository.js
@@ -1,1 +1,141 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * ============================================================
+ *
+ * FILE
+ * MatterRepository.js
+ *
+ * FILE ID
+ * REP-002
+ *
+ * LOCATION
+ * app/repositories/MatterRepository.js
+ *
+ * EXTENDS
+ * BaseRepository
+ *
+ * VERSION
+ * 1.0.0
+ * ============================================================
+ */
 
+import BaseRepository from "./BaseRepository.js";
+
+export default class MatterRepository extends BaseRepository {
+
+    /*=====================================================
+        MAT-REP-001
+        Constructor
+    =====================================================*/
+
+    constructor(storage) {
+
+        super(storage);
+
+    }
+
+    /*=====================================================
+        MAT-REP-002
+        Queries
+    =====================================================*/
+
+    async findByReference(referenceNumber) {
+
+        return this.search({
+
+            referenceNumber
+
+        });
+
+    }
+
+    async findByClient(clientId) {
+
+        return this.search({
+
+            clientId
+
+        });
+
+    }
+
+    async findByConsultant(consultantId) {
+
+        return this.search({
+
+            consultantId
+
+        });
+
+    }
+
+    async findByDepartment(department) {
+
+        return this.search({
+
+            department
+
+        });
+
+    }
+
+    async findByStatus(status) {
+
+        return this.search({
+
+            status
+
+        });
+
+    }
+
+    async findByStage(stage) {
+
+        return this.search({
+
+            stage
+
+        });
+
+    }
+
+    async findByPriority(priority) {
+
+        return this.search({
+
+            priority
+
+        });
+
+    }
+
+    /*=====================================================
+        MAT-REP-003
+        Statistics
+    =====================================================*/
+
+    async statistics() {
+
+        return {
+
+            total: await this.count()
+
+        };
+
+    }
+
+    /*=====================================================
+        MAT-REP-004
+        Reserved
+    =====================================================*/
+
+    fullTextSearch() {}
+
+    archive() {}
+
+    restore() {}
+
+    rebuildIndexes() {}
+
+}
