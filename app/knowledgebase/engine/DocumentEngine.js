5a3a3ccbe2a709b387b859dcd976ec23ef091d73
maliq273 <maliq273@gmail.com>
2026-08-07 11:15:36 +0200

    Update DocumentEngine.js

--- app/knowledgebase/engine/DocumentEngine.js
+++ app/knowledgebase/engine/DocumentEngine.js
@@ -1,1 +1,13 @@
+export default class DocumentEngine{
 
+    static get(data,service){
+
+        return data.services.find(
+
+            s=>s.code===service
+
+        )?.documents || [];
+
+    }
+
+}
