28697891d5694d206c0bb0a38615fe32615095a5
maliq273 <maliq273@gmail.com>
2026-08-07 11:06:28 +0200

    Update KnowledgeEngine.js

--- app/knowledgebase/engine/KnowledgeEngine.js
+++ app/knowledgebase/engine/KnowledgeEngine.js
@@ -1,1 +1,69 @@
+/**
+ * ============================================================
+ * KNOWLEDGE ENGINE
+ * ============================================================
+ */
 
+import KnowledgeLoader from "./KnowledgeLoader.js";
+import KnowledgeSearch from "./KnowledgeSearch.js";
+import RuleEngine from "./RuleEngine.js";
+import RequirementEngine from "./RequirementEngine.js";
+import DocumentEngine from "./DocumentEngine.js";
+
+export default class KnowledgeEngine {
+
+    constructor() {
+
+        this.data = KnowledgeLoader.load();
+
+    }
+
+    search(query){
+
+        return KnowledgeSearch.search(
+
+            this.data,
+
+            query
+
+        );
+
+    }
+
+    getRequirements(service){
+
+        return RequirementEngine.get(
+
+            this.data,
+
+            service
+
+        );
+
+    }
+
+    getRules(service){
+
+        return RuleEngine.get(
+
+            this.data,
+
+            service
+
+        );
+
+    }
+
+    getDocuments(service){
+
+        return DocumentEngine.get(
+
+            this.data,
+
+            service
+
+        );
+
+    }
+
+}
