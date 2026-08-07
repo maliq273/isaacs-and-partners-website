c851db39750fdae2e5325267c40866bf777a098d
maliq273 <maliq273@gmail.com>
2026-08-07 11:15:14 +0200

    Update RequirementEngine.js

--- app/knowledgebase/engine/RequirementEngine.js
+++ app/knowledgebase/engine/RequirementEngine.js
@@ -1,1 +1,13 @@
+export default class RequirementEngine{
 
+    static get(data,service){
+
+        return data.services.find(
+
+            s=>s.code===service
+
+        )?.requirements || [];
+
+    }
+
+}
