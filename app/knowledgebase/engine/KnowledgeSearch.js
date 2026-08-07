5a56cc38a0df93326136af4b71a59ebe7034c080
maliq273 <maliq273@gmail.com>
2026-08-07 11:13:52 +0200

    Update KnowledgeSearch.js

--- app/knowledgebase/engine/KnowledgeSearch.js
+++ app/knowledgebase/engine/KnowledgeSearch.js
@@ -1,1 +1,23 @@
+export default class KnowledgeSearch{
 
+    static search(data,query){
+
+        const q=query.toLowerCase();
+
+        return data.services.filter(service=>{
+
+            return (
+
+                service.name.toLowerCase().includes(q)
+
+                ||
+
+                service.category.toLowerCase().includes(q)
+
+            );
+
+        });
+
+    }
+
+}
