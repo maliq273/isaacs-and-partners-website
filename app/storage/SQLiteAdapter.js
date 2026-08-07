de1f5f6f1ad2b77519c973fedb63a38d393b9878
maliq273 <maliq273@gmail.com>
2026-08-07 15:23:14 +0200

    Update SQLiteAdapter.js

--- app/storage/SQLiteAdapter.js
+++ app/storage/SQLiteAdapter.js
@@ -1,1 +1,76 @@
+/**
+ * ============================================================
+ * FILE: SQLiteAdapter.js
+ * ID: STO-008
+ * LOCATION: app/storage/SQLiteAdapter.js
+ * ============================================================
+ */
 
+export default class SQLiteAdapter {
+
+    constructor(config = {}) {
+
+        this.database = config.database || "isaacs.db";
+
+        this.connected = false;
+
+    }
+
+    /*=====================================================
+        SQL-001
+        Connection
+    =====================================================*/
+
+    async connect() {
+
+        this.connected = true;
+
+        return true;
+
+    }
+
+    async disconnect() {
+
+        this.connected = false;
+
+    }
+
+    /*=====================================================
+        SQL-002
+        CRUD
+    =====================================================*/
+
+    async create(entity) {}
+
+    async findById(id) {}
+
+    async findAll() {}
+
+    async update(entity) {}
+
+    async delete(id) {}
+
+    async exists(id) {}
+
+    async count() {}
+
+    async search(criteria) {}
+
+    async filter(filters) {}
+
+    async paginate(page, size) {}
+
+    async sort(field, direction) {}
+
+    /*=====================================================
+        SQL-003
+        Transactions
+    =====================================================*/
+
+    beginTransaction() {}
+
+    commitTransaction() {}
+
+    rollbackTransaction() {}
+
+}
