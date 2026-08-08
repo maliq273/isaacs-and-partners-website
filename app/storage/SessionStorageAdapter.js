682fa346d3f38aea991bbf86798f34870a7be560
maliq273 <maliq273@gmail.com>
2026-08-08 12:47:15 +0200

    Update SessionStorageAdapter.js

--- app/storage/SessionStorageAdapter.js
+++ app/storage/SessionStorageAdapter.js
@@ -1,1 +1,41 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * SessionStorageAdapter
+ * ============================================================
+ */
 
+import LocalStorageAdapter
+    from "./LocalStorageAdapter.js";
+
+export default class SessionStorageAdapter
+    extends LocalStorageAdapter {
+
+    constructor(options = {}) {
+
+        super({
+            ...options,
+            name: "SessionStorageAdapter",
+            storage:
+                options.storage ??
+                globalThis.sessionStorage,
+            prefix:
+                options.prefix ??
+                "isaacs:session:"
+        });
+
+        this.name =
+            "SessionStorageAdapter";
+
+    }
+
+
+    // =========================================================
+    // FUTURE INSERT
+    // Session expiry
+    // Idle timeout
+    // Automatic logout
+    // Supervisor user-log integration
+    // =========================================================
+
+}
