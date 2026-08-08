fba660978f1c7bb297023cff950ce918e7936287
maliq273 <maliq273@gmail.com>
2026-08-08 14:04:35 +0200

    Update SuccessResult.js

--- app/results/SuccessResult.js
+++ app/results/SuccessResult.js
@@ -1,1 +1,59 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * SuccessResult
+ * ============================================================
+ */
 
+import Result from "./Result.js";
+
+export default class SuccessResult
+    extends Result {
+
+    constructor(data = {}) {
+
+        super({
+
+            ...data,
+
+            success: true,
+
+            code:
+                data.code ??
+                "SUCCESS"
+
+        });
+
+        // ====================================================
+        // FUTURE INSERT
+        //
+        // Created result
+        // Updated result
+        // Deleted result
+        // Submitted result
+        // Completed result
+        //
+        // ====================================================
+    }
+
+
+    static create(
+        data = null,
+        message =
+            "Operation completed successfully.",
+        options = {}
+    ) {
+
+        return new SuccessResult({
+
+            data,
+
+            message,
+
+            ...options
+
+        });
+
+    }
+
+}
