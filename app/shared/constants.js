14529f9103a495ed954f20b1ceff1465dae2c596
maliq273 <maliq273@gmail.com>
2026-08-08 13:11:59 +0200

    Update constants.js

--- app/shared/constants.js
+++ app/shared/constants.js
@@ -1,1 +1,76 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Shared Constants
+ * ------------------------------------------------------------
+ * Central export point for shared constants.
+ * ============================================================
+ */
 
+export {
+    AppointmentType
+} from "./constants/AppointmentType.js";
+
+export {
+    CommunicationType
+} from "./constants/CommunicationType.js";
+
+export {
+    Complexity
+} from "./constants/Complexity.js";
+
+export {
+    Countries
+} from "./constants/Countries.js";
+
+export {
+    Department
+} from "./constants/Department.js";
+
+export {
+    DocumentType
+} from "./constants/DocumentType.js";
+
+export {
+    Languages
+} from "./constants/Languages.js";
+
+export {
+    Permissions
+} from "./constants/Permissions.js";
+
+export {
+    Priority
+} from "./constants/Priority.js";
+
+export {
+    RiskLevel
+} from "./constants/RiskLevel.js";
+
+export {
+    Roles
+} from "./constants/Roles.js";
+
+export {
+    ServiceCategory
+} from "./constants/ServiceCategory.js";
+
+export {
+    Status
+} from "./constants/Status.js";
+
+export {
+    ValidationRules
+} from "./constants/ValidationRules.js";
+
+export {
+    Workflow
+} from "./constants/Workflow.js";
+
+
+// ============================================================
+// FUTURE INSERT
+//
+// New shared constants MUST be exported here.
+//
+// ============================================================
