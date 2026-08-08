8429096819cc41e5f3db5b726e2ce4f44ea69325
maliq273 <maliq273@gmail.com>
2026-08-08 13:00:00 +0200

    Update Countries.js

--- app/shared/constants/Countries.js
+++ app/shared/constants/Countries.js
@@ -1,1 +1,87 @@
+/**
+ * ============================================================
+ * ISAACS & PARTNERS ENTERPRISE PLATFORM
+ * Countries
+ * ============================================================
+ *
+ * ISO-oriented country catalogue.
+ */
 
+export const Countries = Object.freeze({
+
+    SOUTH_AFRICA: "ZA",
+
+    ZIMBABWE: "ZW",
+
+    MOZAMBIQUE: "MZ",
+
+    NAMIBIA: "NA",
+
+    BOTSWANA: "BW",
+
+    ESWATINI: "SZ",
+
+    LESOTHO: "LS",
+
+    MALAWI: "MW",
+
+    ZAMBIA: "ZM",
+
+    ANGOLA: "AO",
+
+    DEMOCRATIC_REPUBLIC_OF_CONGO: "CD",
+
+    NIGERIA: "NG",
+
+    GHANA: "GH",
+
+    KENYA: "KE",
+
+    TANZANIA: "TZ",
+
+    UGANDA: "UG",
+
+    ETHIOPIA: "ET",
+
+    INDIA: "IN",
+
+    PAKISTAN: "PK",
+
+    BANGLADESH: "BD",
+
+    CHINA: "CN",
+
+    UNITED_KINGDOM: "GB",
+
+    UNITED_STATES: "US",
+
+    CANADA: "CA",
+
+    AUSTRALIA: "AU",
+
+    NEW_ZEALAND: "NZ",
+
+    GERMANY: "DE",
+
+    FRANCE: "FR",
+
+    NETHERLANDS: "NL",
+
+    PORTUGAL: "PT",
+
+    SPAIN: "ES",
+
+    ITALY: "IT",
+
+    OTHER: "OTHER"
+
+    // ========================================================
+    // FUTURE INSERT
+    // Complete ISO-3166 country catalogue
+    // Nationality mappings
+    // Visa-country restrictions
+    // ========================================================
+
+});
+
+export default Countries;
