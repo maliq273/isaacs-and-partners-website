ba23236e615cca68af7fb43c9cf1440347b1322e
maliq273 <maliq273@gmail.com>
2026-08-13 19:34:37 +0200

    Update ValidationException.js

--- app/exceptions/ValidationException.js
+++ app/exceptions/ValidationException.js
@@ -1,1 +1,39 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * ValidationException
+ * ------------------------------------------------------------
+ * Input, domain, document, workflow, or business validation
+ * failure.
+ */
+
+export class ValidationException extends ApplicationException {
+    constructor(
+        message = "Validation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "VALIDATION_ERROR",
+            status:
+                options.status || 422
+        });
+
+        this.name =
+            "ValidationException";
+
+        this.fields =
+            options.fields || {};
+    }
+
+    toJSON() {
+        return {
+            ...super.toJSON(),
+            fields: this.fields
+        };
+    }
+}
+
+export default ValidationException;
