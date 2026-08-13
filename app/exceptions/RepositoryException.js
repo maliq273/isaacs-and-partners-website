8cd0aeb71f3d1ac6a8e8ddf6d6b22224481373ea
maliq273 <maliq273@gmail.com>
2026-08-13 19:34:09 +0200

    Update RepositoryException.js

--- app/exceptions/RepositoryException.js
+++ app/exceptions/RepositoryException.js
@@ -1,1 +1,28 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * RepositoryException
+ * ------------------------------------------------------------
+ * Persistence/repository operation failure.
+ */
+
+export class RepositoryException extends ApplicationException {
+    constructor(
+        message = "Repository operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "REPOSITORY_ERROR",
+            status:
+                options.status || 500
+        });
+
+        this.name =
+            "RepositoryException";
+    }
+}
+
+export default RepositoryException;
