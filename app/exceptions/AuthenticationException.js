dc537f10f465aa828a4eb9d61f5422d765867069
maliq273 <maliq273@gmail.com>
2026-08-13 19:31:30 +0200

    Update AuthenticationException.js

--- app/exceptions/AuthenticationException.js
+++ app/exceptions/AuthenticationException.js
@@ -1,1 +1,28 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * AuthenticationException
+ * ------------------------------------------------------------
+ * Authentication failed or authentication state is invalid.
+ */
+
+export class AuthenticationException extends ApplicationException {
+    constructor(
+        message = "Authentication failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "AUTHENTICATION_ERROR",
+            status:
+                options.status || 401
+        });
+
+        this.name =
+            "AuthenticationException";
+    }
+}
+
+export default AuthenticationException;
