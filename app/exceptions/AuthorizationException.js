8e10a7eee36c5956664980d0af2dabcbdeca638d
maliq273 <maliq273@gmail.com>
2026-08-13 19:31:46 +0200

    Update AuthorizationException.js

--- app/exceptions/AuthorizationException.js
+++ app/exceptions/AuthorizationException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * AuthorizationException
+ * ------------------------------------------------------------
+ * Authenticated user does not have permission to perform the
+ * requested operation.
+ */
+
+export class AuthorizationException extends ApplicationException {
+    constructor(
+        message = "Access denied",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "AUTHORIZATION_ERROR",
+            status:
+                options.status || 403
+        });
+
+        this.name =
+            "AuthorizationException";
+    }
+}
+
+export default AuthorizationException;
