7ae53f4065a97e66a209f0dd90f3b90cbb9de0ba
maliq273 <maliq273@gmail.com>
2026-08-13 19:24:27 +0200

    Update BuilderFactory.js

--- app/factories/BuilderFactory.js
+++ app/factories/BuilderFactory.js
@@ -1,1 +1,70 @@
+/**
+ * BuilderFactory
+ * ------------------------------------------------------------
+ * Central factory for constructing configured builders.
+ *
+ * A builder may be:
+ * - pipeline builder
+ * - query builder
+ * - workflow builder
+ * - document/bundle builder
+ */
 
+export class BuilderFactory {
+    constructor({
+        builders = {},
+        logger = console
+    } = {}) {
+        this.builders = {
+            ...builders
+        };
+
+        this.logger = logger;
+    }
+
+    register(name, Builder) {
+        if (
+            !name ||
+            typeof Builder !==
+                "function"
+        ) {
+            throw new TypeError(
+                "Builder name and constructor are required"
+            );
+        }
+
+        this.builders[name] =
+            Builder;
+
+        return this;
+    }
+
+    create(name, options = {}) {
+        const Builder =
+            this.builders[name];
+
+        if (!Builder) {
+            throw new Error(
+                `Builder is not registered: ${name}`
+            );
+        }
+
+        return new Builder(
+            options
+        );
+    }
+
+    has(name) {
+        return Boolean(
+            this.builders[name]
+        );
+    }
+
+    list() {
+        return Object.keys(
+            this.builders
+        );
+    }
+}
+
+export default BuilderFactory;
