ae89086e04720e6966d3a4571336c244bb504053
maliq273 <maliq273@gmail.com>
2026-08-13 19:34:23 +0200

    Update StorageException.js

--- app/exceptions/StorageException.js
+++ app/exceptions/StorageException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * StorageException
+ * ------------------------------------------------------------
+ * Local storage, IndexedDB, SQLite, Supabase, backup,
+ * encryption, migration, or restore failure.
+ */
+
+export class StorageException extends ApplicationException {
+    constructor(
+        message = "Storage operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "STORAGE_ERROR",
+            status:
+                options.status || 500
+        });
+
+        this.name =
+            "StorageException";
+    }
+}
+
+export default StorageException;
