d37dad4f98620bfbedd19b85ce4e7eb7599fab23
maliq273 <maliq273@gmail.com>
2026-08-13 19:33:52 +0200

    Update NetworkException.js

--- app/exceptions/NetworkException.js
+++ app/exceptions/NetworkException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * NetworkException
+ * ------------------------------------------------------------
+ * External API, HTTP, synchronization, or network operation
+ * failure.
+ */
+
+export class NetworkException extends ApplicationException {
+    constructor(
+        message = "Network operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "NETWORK_ERROR",
+            status:
+                options.status || 503
+        });
+
+        this.name =
+            "NetworkException";
+    }
+}
+
+export default NetworkException;
