8a343e67711851d9b94d4fe1a799acf49adc4b64
maliq273 <maliq273@gmail.com>
2026-08-13 19:31:59 +0200

    Update BusinessRuleException.js

--- app/exceptions/BusinessRuleException.js
+++ app/exceptions/BusinessRuleException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * BusinessRuleException
+ * ------------------------------------------------------------
+ * Raised when an operation violates an application/business
+ * rule.
+ */
+
+export class BusinessRuleException extends ApplicationException {
+    constructor(
+        message = "Business rule violated",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "BUSINESS_RULE_ERROR",
+            status:
+                options.status || 422
+        });
+
+        this.name =
+            "BusinessRuleException";
+    }
+}
+
+export default BusinessRuleException;
