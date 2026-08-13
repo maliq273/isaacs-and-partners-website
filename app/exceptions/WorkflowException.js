d877137d219c4592979b12a3889bc5d9f6853534
maliq273 <maliq273@gmail.com>
2026-08-13 19:34:52 +0200

    Update WorkflowException.js

--- app/exceptions/WorkflowException.js
+++ app/exceptions/WorkflowException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * WorkflowException
+ * ------------------------------------------------------------
+ * Workflow creation, transition, execution, or state-machine
+ * failure.
+ */
+
+export class WorkflowException extends ApplicationException {
+    constructor(
+        message = "Workflow operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "WORKFLOW_ERROR",
+            status:
+                options.status || 422
+        });
+
+        this.name =
+            "WorkflowException";
+    }
+}
+
+export default WorkflowException;
