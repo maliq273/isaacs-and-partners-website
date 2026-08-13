3a25a50b079c8607df639105bc194a7056f5e8fa
maliq273 <maliq273@gmail.com>
2026-08-13 19:32:46 +0200

    Update DocumentException.js

--- app/exceptions/DocumentException.js
+++ app/exceptions/DocumentException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * DocumentException
+ * ------------------------------------------------------------
+ * Document processing, validation, OCR, generation, storage,
+ * or document workflow failure.
+ */
+
+export class DocumentException extends ApplicationException {
+    constructor(
+        message = "Document operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "DOCUMENT_ERROR",
+            status:
+                options.status || 422
+        });
+
+        this.name =
+            "DocumentException";
+    }
+}
+
+export default DocumentException;
