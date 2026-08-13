e3757e61670ed6eca1be2020830bd937320f6d3e
maliq273 <maliq273@gmail.com>
2026-08-13 19:33:36 +0200

    Update KnowledgeException.js

--- app/exceptions/KnowledgeException.js
+++ app/exceptions/KnowledgeException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * KnowledgeException
+ * ------------------------------------------------------------
+ * Knowledgebase loading, indexing, searching, validation,
+ * rules, or source processing failure.
+ */
+
+export class KnowledgeException extends ApplicationException {
+    constructor(
+        message = "Knowledgebase operation failed",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "KNOWLEDGE_ERROR",
+            status:
+                options.status || 500
+        });
+
+        this.name =
+            "KnowledgeException";
+    }
+}
+
+export default KnowledgeException;
