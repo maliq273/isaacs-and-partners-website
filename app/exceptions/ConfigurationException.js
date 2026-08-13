e0aebf242f308b866d7faef28a1cb99d7609627e
maliq273 <maliq273@gmail.com>
2026-08-13 19:32:13 +0200

    Update ConfigurationException.js

--- app/exceptions/ConfigurationException.js
+++ app/exceptions/ConfigurationException.js
@@ -1,1 +1,29 @@
+import ApplicationException from "./ApplicationException.js";
 
+/**
+ * ConfigurationException
+ * ------------------------------------------------------------
+ * Application configuration or dependency configuration is
+ * invalid or incomplete.
+ */
+
+export class ConfigurationException extends ApplicationException {
+    constructor(
+        message = "Invalid application configuration",
+        options = {}
+    ) {
+        super(message, {
+            ...options,
+            code:
+                options.code ||
+                "CONFIGURATION_ERROR",
+            status:
+                options.status || 500
+        });
+
+        this.name =
+            "ConfigurationException";
+    }
+}
+
+export default ConfigurationException;
