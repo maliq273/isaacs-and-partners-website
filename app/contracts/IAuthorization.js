55240d0512d9f179cf47394590e63478985bcaf7
maliq273 <maliq273@gmail.com>
2026-08-14 15:39:09 +0200

    Update IAuthorization.js

--- app/contracts/IAuthorization.js
+++ app/contracts/IAuthorization.js
@@ -1,1 +1,32 @@
+/**
+ * Isaacs and Partners
+ * Contract: Authorization
+ */
 
+export class IAuthorization {
+  can(user, action, resource = null) {
+    throw new Error(
+      "IAuthorization.can() must be implemented."
+    );
+  }
+
+  cannot(user, action, resource = null) {
+    throw new Error(
+      "IAuthorization.cannot() must be implemented."
+    );
+  }
+
+  authorize(user, action, resource = null) {
+    throw new Error(
+      "IAuthorization.authorize() must be implemented."
+    );
+  }
+
+  getPermissions(user) {
+    throw new Error(
+      "IAuthorization.getPermissions() must be implemented."
+    );
+  }
+}
+
+export default IAuthorization;
