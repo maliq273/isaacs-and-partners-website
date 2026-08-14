ded790d8395976568d56ad9811183e51fdff29b2
maliq273 <maliq273@gmail.com>
2026-08-14 10:24:38 +0200

    Update EventDispatcher.js

--- app/events/EventDispatcher.js
+++ app/events/EventDispatcher.js
@@ -1,1 +1,50 @@
+/**
+ * EventHandler
+ * ------------------------------------------------------------
+ * Base class for event handlers.
+ *
+ * A handler should implement:
+ *
+ * async handle(event)
+ */
 
+export class EventHandler {
+    constructor({
+        name = null,
+        logger = console
+    } = {}) {
+        this.name =
+            name ||
+            this.constructor.name;
+
+        this.logger =
+            logger;
+    }
+
+    supports(event) {
+        return true;
+    }
+
+    async handle() {
+        throw new Error(
+            `${this.name} must implement handle(event)`
+        );
+    }
+
+    async execute(event) {
+        if (
+            !this.supports(event)
+        ) {
+            return {
+                handled: false,
+                skipped: true
+            };
+        }
+
+        return this.handle(
+            event
+        );
+    }
+}
+
+export default EventHandler;
