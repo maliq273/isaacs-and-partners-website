1674578ffaef0cf29a7a544a68f9439d831a86df
maliq273 <maliq273@gmail.com>
2026-08-14 11:22:43 +0200

    Update SortBuilder.js

--- app/database/query/SortBuilder.js
+++ app/database/query/SortBuilder.js
@@ -1,1 +1,49 @@
+import {
+    quoteIdentifier
+} from "../functions/DatabaseFunctions.js";
 
+export default class SortBuilder {
+    constructor() {
+        this.sorts = [];
+    }
+
+    add(
+        column,
+        direction = "ASC"
+    ) {
+        const normalizedDirection =
+            String(direction)
+                .toUpperCase() ===
+            "DESC"
+                ? "DESC"
+                : "ASC";
+
+        this.sorts.push(
+            `${quoteIdentifier(column)} ${normalizedDirection}`
+        );
+
+        return this;
+    }
+
+    ascending(column) {
+        return this.add(
+            column,
+            "ASC"
+        );
+    }
+
+    descending(column) {
+        return this.add(
+            column,
+            "DESC"
+        );
+    }
+
+    build(
+        fallback = '"created_at" DESC'
+    ) {
+        return this.sorts.length
+            ? this.sorts.join(", ")
+            : fallback;
+    }
+}
