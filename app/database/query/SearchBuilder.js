f5a1106d94c12f2fc0163af4e6e5a16cd547359b
maliq273 <maliq273@gmail.com>
2026-08-14 11:23:02 +0200

    Update SearchBuilder.js

--- app/database/query/SearchBuilder.js
+++ app/database/query/SearchBuilder.js
@@ -1,1 +1,49 @@
+import {
+    buildMultiColumnSearch,
+    createSearchPattern
+} from "../functions/SearchFunctions.js";
 
+export default class SearchBuilder {
+    constructor() {
+        this.columns = [];
+        this.parameter = "search";
+        this.value = null;
+    }
+
+    addColumn(column) {
+        this.columns.push(column);
+        return this;
+    }
+
+    addColumns(columns = []) {
+        for (const column of columns) {
+            this.addColumn(column);
+        }
+
+        return this;
+    }
+
+    setValue(value) {
+        this.value = value;
+        return this;
+    }
+
+    build() {
+        const condition =
+            buildMultiColumnSearch({
+                columns: this.columns,
+                parameter:
+                    this.parameter
+            });
+
+        return {
+            sql: condition.sql,
+            parameters: {
+                [this.parameter]:
+                    createSearchPattern(
+                        this.value || ""
+                    )
+            }
+        };
+    }
+}
