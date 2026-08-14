85bd4313a27c3d2b8e3d1418ef44261c72451610
maliq273 <maliq273@gmail.com>
2026-08-14 11:28:20 +0200

    Update AppointmentTable.js

--- app/database/models/AppointmentTable.js
+++ app/database/models/AppointmentTable.js
@@ -1,1 +1,21 @@
+export default class AppointmentTable {
+    static tableName = "appointments";
 
+    static primaryKey = "id";
+
+    static columns = [
+        "id",
+        "client_id",
+        "matter_id",
+        "user_id",
+        "appointment_type",
+        "appointment_date",
+        "start_time",
+        "end_time",
+        "status",
+        "location",
+        "notes",
+        "created_at",
+        "updated_at"
+    ];
+}
