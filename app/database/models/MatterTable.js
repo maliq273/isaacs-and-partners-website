8697396c04c170d966531ea7c5acf20245e33b4c
maliq273 <maliq273@gmail.com>
2026-08-14 11:29:10 +0200

    Update MatterTable.js

--- app/database/models/MatterTable.js
+++ app/database/models/MatterTable.js
@@ -1,1 +1,24 @@
+export default class MatterTable {
+    static tableName = "matters";
 
+    static primaryKey = "id";
+
+    static columns = [
+        "id",
+        "client_id",
+        "reference_number",
+        "title",
+        "type",
+        "department",
+        "stage",
+        "status",
+        "priority",
+        "assigned_to",
+        "source",
+        "outcome",
+        "opened_at",
+        "closed_at",
+        "created_at",
+        "updated_at"
+    ];
+}
