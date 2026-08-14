bba0252c4d1533db1000d58ada114f6fa07a7c32
maliq273 <maliq273@gmail.com>
2026-08-14 15:40:21 +0200

    Update IManager.js

--- app/contracts/IManager.js
+++ app/contracts/IManager.js
@@ -1,1 +1,38 @@
+/**
+ * Isaacs and Partners
+ * Contract: Manager
+ */
 
+export class IManager {
+  create(data) {
+    throw new Error(
+      "IManager.create() must be implemented."
+    );
+  }
+
+  get(id) {
+    throw new Error(
+      "IManager.get() must be implemented."
+    );
+  }
+
+  update(id, data) {
+    throw new Error(
+      "IManager.update() must be implemented."
+    );
+  }
+
+  delete(id) {
+    throw new Error(
+      "IManager.delete() must be implemented."
+    );
+  }
+
+  list(filters = {}) {
+    throw new Error(
+      "IManager.list() must be implemented."
+    );
+  }
+}
+
+export default IManager;
