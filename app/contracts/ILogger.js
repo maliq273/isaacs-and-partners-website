91f1579298ca8847fb05162a400d349b7f4b8215
maliq273 <maliq273@gmail.com>
2026-08-14 15:40:03 +0200

    Update ILogger.js

--- app/contracts/ILogger.js
+++ app/contracts/ILogger.js
@@ -1,1 +1,38 @@
+/**
+ * Isaacs and Partners
+ * Contract: Logger
+ */
 
+export class ILogger {
+  debug(message, metadata = {}) {
+    throw new Error(
+      "ILogger.debug() must be implemented."
+    );
+  }
+
+  info(message, metadata = {}) {
+    throw new Error(
+      "ILogger.info() must be implemented."
+    );
+  }
+
+  warn(message, metadata = {}) {
+    throw new Error(
+      "ILogger.warn() must be implemented."
+    );
+  }
+
+  error(message, metadata = {}) {
+    throw new Error(
+      "ILogger.error() must be implemented."
+    );
+  }
+
+  audit(message, metadata = {}) {
+    throw new Error(
+      "ILogger.audit() must be implemented."
+    );
+  }
+}
+
+export default ILogger;
