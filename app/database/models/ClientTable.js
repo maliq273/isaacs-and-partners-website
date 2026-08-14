9005117eed35f28fbe5a453902a9de08fd7641fd
maliq273 <maliq273@gmail.com>
2026-08-14 11:26:24 +0200

    Update ClientTable.js

--- app/database/models/ClientTable.js
+++ app/database/models/ClientTable.js
@@ -1,1 +1,21 @@
+export default class ClientTable {
+    static tableName = "clients";
 
+    static primaryKey = "id";
+
+    static columns = [
+        "id",
+        "matter_number",
+        "first_name",
+        "last_name",
+        "email",
+        "phone",
+        "passport_number",
+        "nationality",
+        "date_of_birth",
+        "status",
+        "notes",
+        "created_at",
+        "updated_at"
+    ];
+}
