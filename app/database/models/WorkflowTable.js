7e83d92ed9b35e28e6bfa694f3ff4d072c02d366
maliq273 <maliq273@gmail.com>
2026-08-14 11:29:29 +0200

    Update WorkflowTable.js

--- app/database/models/WorkflowTable.js
+++ app/database/models/WorkflowTable.js
@@ -1,1 +1,18 @@
+export default class WorkflowTable {
+    static tableName = "workflows";
 
+    static primaryKey = "id";
+
+    static columns = [
+        "id",
+        "matter_id",
+        "type",
+        "status",
+        "current_step",
+        "assigned_to",
+        "started_at",
+        "completed_at",
+        "created_at",
+        "updated_at"
+    ];
+}
