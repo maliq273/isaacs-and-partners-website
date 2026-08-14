f86f34ba4a2e6b1659e42b17e9a5c22072eea8d7
maliq273 <maliq273@gmail.com>
2026-08-14 15:42:25 +0200

    Update IService.js

--- app/contracts/IService.js
+++ app/contracts/IService.js
@@ -1,1 +1,44 @@
+/**
+ * Isaacs and Partners
+ * Contract: Service
+ */
 
+export class IService {
+  initialise(context = {}) {
+    throw new Error(
+      "IService.initialise() must be implemented."
+    );
+  }
+
+  create(data, context = {}) {
+    throw new Error(
+      "IService.create() must be implemented."
+    );
+  }
+
+  get(id, context = {}) {
+    throw new Error(
+      "IService.get() must be implemented."
+    );
+  }
+
+  update(id, data, context = {}) {
+    throw new Error(
+      "IService.update() must be implemented."
+    );
+  }
+
+  delete(id, context = {}) {
+    throw new Error(
+      "IService.delete() must be implemented."
+    );
+  }
+
+  list(filters = {}, context = {}) {
+    throw new Error(
+      "IService.list() must be implemented."
+    );
+  }
+}
+
+export default IService;
