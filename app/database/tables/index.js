f4bb8c469e7fe95cd09f1214d8ae9523d0532745
maliq273 <maliq273@gmail.com>
2026-08-14 12:04:52 +0200

    Create index.js

---
+++ app/database/tables/index.js
@@ -0,0 +1,53 @@
+export {
+    default as TableDefinition
+} from "./TableDefinition.js";
+
+export {
+    default as UsersTable
+} from "./UsersTable.js";
+
+export {
+    default as CompaniesTable
+} from "./CompaniesTable.js";
+
+export {
+    default as ClientsTable
+} from "./ClientsTable.js";
+
+export {
+    default as MattersTable
+} from "./MattersTable.js";
+
+export {
+    default as AppointmentsTable
+} from "./AppointmentsTable.js";
+
+export {
+    default as DocumentsTable
+} from "./DocumentsTable.js";
+
+export {
+    default as InvoicesTable
+} from "./InvoicesTable.js";
+
+export {
+    default as PaymentsTable
+} from "./PaymentsTable.js";
+
+export {
+    default as TasksTable
+} from "./TasksTable.js";
+
+export {
+    default as WorkflowsTable
+} from "./WorkflowsTable.js";
+
+export {
+    default as KnowledgeTable
+} from "./KnowledgeTable.js";
+
+export {
+    getTable,
+    hasTable,
+    getTableNames
+} from "./TableRegistry.js";
