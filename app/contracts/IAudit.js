40f966b34328f9682d5dfcd89651b0de9425d900
maliq273 <maliq273@gmail.com>
2026-08-14 15:38:54 +0200

    Update IAudit.js

--- app/contracts/IAudit.js
+++ app/contracts/IAudit.js
@@ -1,1 +1,35 @@
+/**
+ * Isaacs and Partners
+ * Contract: Audit
+ *
+ * Defines the minimum interface expected from an
+ * audit implementation.
+ */
 
+export class IAudit {
+  record(event) {
+    throw new Error(
+      "IAudit.record() must be implemented."
+    );
+  }
+
+  log(action, context = {}) {
+    throw new Error(
+      "IAudit.log() must be implemented."
+    );
+  }
+
+  query(filters = {}) {
+    throw new Error(
+      "IAudit.query() must be implemented."
+    );
+  }
+
+  getById(id) {
+    throw new Error(
+      "IAudit.getById() must be implemented."
+    );
+  }
+}
+
+export default IAudit;
