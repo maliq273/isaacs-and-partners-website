265268c4225d16f1ef18c023aa1881163a552c85
maliq273 <maliq273@gmail.com>
2026-08-14 12:50:39 +0200

    Create index.js

---
+++ app/database/triggers/index.js
@@ -0,0 +1,15 @@
+export const triggerFiles = Object.freeze([
+    "updated_at.sql",
+    "invoice_balance.sql",
+    "matter_status.sql",
+    "workflow_status.sql",
+    "document_status.sql",
+    "task_status.sql",
+    "audit.sql",
+    "timeline.sql",
+    "communications.sql",
+    "cleanup.sql",
+    "index.sql"
+]);
+
+export default triggerFiles;
