c84d75d76dbc9b566ce8af14ffe27e35614e3bef
maliq273 <maliq273@gmail.com>
2026-08-14 11:43:41 +0200

    Create index.sql

---
+++ app/database/schema/index.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys = ON;
+
+.read User.sql
+.read Company.sql
+.read Client.sql
+.read Matter.sql
+.read Appointment.sql
+.read Document.sql
+.read Invoice.sql
+.read Payment.sql
+.read Task.sql
+.read Workflow.sql
+.read Knowledge.sql
