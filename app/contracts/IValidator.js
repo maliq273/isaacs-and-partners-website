065909a83b3de66baa5261fba67ae87af197afb8
maliq273 <maliq273@gmail.com>
2026-08-14 15:43:23 +0200

    Update IValidator.js

--- app/contracts/IValidator.js
+++ app/contracts/IValidator.js
@@ -1,1 +1,32 @@
+/**
+ * Isaacs and Partners
+ * Contract: Validator
+ */
 
+export class IValidator {
+  validate(data, context = {}) {
+    throw new Error(
+      "IValidator.validate() must be implemented."
+    );
+  }
+
+  isValid(data, context = {}) {
+    throw new Error(
+      "IValidator.isValid() must be implemented."
+    );
+  }
+
+  getErrors() {
+    throw new Error(
+      "IValidator.getErrors() must be implemented."
+    );
+  }
+
+  clearErrors() {
+    throw new Error(
+      "IValidator.clearErrors() must be implemented."
+    );
+  }
+}
+
+export default IValidator;
