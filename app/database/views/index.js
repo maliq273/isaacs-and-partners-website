6c280f99f46f7b1dec1f9dac27f844fe3e58b191
maliq273 <maliq273@gmail.com>
2026-08-14 12:58:41 +0200

    Create index.js

---
+++ app/database/views/index.js
@@ -0,0 +1,19 @@
+export const viewFiles = Object.freeze([
+    "dashboard_overview.sql",
+    "client_overview.sql",
+    "matter_overview.sql",
+    "document_overview.sql",
+    "task_overview.sql",
+    "workflow_overview.sql",
+    "invoice_overview.sql",
+    "payment_overview.sql",
+    "timeline_overview.sql",
+    "audit_overview.sql",
+    "communication_overview.sql",
+    "reporting_overview.sql",
+    "matter_financial_summary.sql",
+    "matter_activity_summary.sql",
+    "knowledge_overview.sql"
+]);
+
+export default viewFiles;
