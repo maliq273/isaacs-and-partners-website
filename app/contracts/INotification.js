eb6daed735c21debf0efaa73c2a65dd3d125869d
maliq273 <maliq273@gmail.com>
2026-08-14 15:41:12 +0200

    Create INotification class with required methods
    Define INotification class with unimplemented methods for sending notifications and managing read status.

--- app/contracts/INotification.js
+++ app/contracts/INotification.js
@@ -1,1 +1,38 @@
+/**
+ * Isaacs and Partners
+ * Contract: Notification
+ */
 
+export class INotification {
+  send(notification) {
+    throw new Error(
+      "INotification.send() must be implemented."
+    );
+  }
+
+  sendToUser(userId, notification) {
+    throw new Error(
+      "INotification.sendToUser() must be implemented."
+    );
+  }
+
+  sendToMatter(matterId, notification) {
+    throw new Error(
+      "INotification.sendToMatter() must be implemented."
+    );
+  }
+
+  markAsRead(notificationId) {
+    throw new Error(
+      "INotification.markAsRead() must be implemented."
+    );
+  }
+
+  getUnread(userId) {
+    throw new Error(
+      "INotification.getUnread() must be implemented."
+    );
+  }
+}
+
+export default INotification;
