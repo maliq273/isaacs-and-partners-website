8c2419c75ab47dbc70b125531c967b5b1f797902
maliq273 <maliq273@gmail.com>
2026-08-14 15:39:27 +0200

    Update IBuilder.js

--- app/contracts/IBuilder.js
+++ app/contracts/IBuilder.js
@@ -1,1 +1,20 @@
+/**
+ * Isaacs and Partners
+ * Contract: Builder
+ */
 
+export class IBuilder {
+  reset() {
+    throw new Error(
+      "IBuilder.reset() must be implemented."
+    );
+  }
+
+  build() {
+    throw new Error(
+      "IBuilder.build() must be implemented."
+    );
+  }
+}
+
+export default IBuilder;
