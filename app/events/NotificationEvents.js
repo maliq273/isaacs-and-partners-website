163cc526e10123e69b8a875d6b338153b64ace21
maliq273 <maliq273@gmail.com>
2026-08-14 10:27:15 +0200

    Add NotificationEvents for lifecycle events

--- app/events/NotificationEvents.js
+++ app/events/NotificationEvents.js
@@ -1,1 +1,45 @@
+/**
+ * NotificationEvents
+ * ------------------------------------------------------------
+ * Notification lifecycle events.
+ */
 
+export const NotificationEvents = Object.freeze({
+    CREATED:
+        "notification.created",
+
+    QUEUED:
+        "notification.queued",
+
+    SENT:
+        "notification.sent",
+
+    FAILED:
+        "notification.failed",
+
+    READ:
+        "notification.read",
+
+    DISMISSED:
+        "notification.dismissed",
+
+    REMINDER_CREATED:
+        "notification.reminder.created",
+
+    REMINDER_SENT:
+        "notification.reminder.sent",
+
+    APPOINTMENT_REMINDER:
+        "notification.appointment.reminder",
+
+    DOCUMENT_OUTSTANDING:
+        "notification.document.outstanding",
+
+    MATTER_STATUS_CHANGED:
+        "notification.matter.status.changed",
+
+    PAYMENT_DUE:
+        "notification.payment.due"
+});
+
+export default NotificationEvents;
