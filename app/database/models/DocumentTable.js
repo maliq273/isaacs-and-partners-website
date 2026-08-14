54ab8bab651afc623b5d8a784055bc0544f30bd5
maliq273 <maliq273@gmail.com>
2026-08-14 11:28:51 +0200

    Update DocumentTable.js

--- app/database/models/DocumentTable.js
+++ app/database/models/DocumentTable.js
@@ -1,1 +1,24 @@
+export default class DocumentTable {
+    static tableName = "documents";
 
+    static primaryKey = "id";
+
+    static columns = [
+        "id",
+        "client_id",
+        "matter_id",
+        "document_type",
+        "name",
+        "file_name",
+        "file_path",
+        "mime_type",
+        "file_size",
+        "file_hash",
+        "status",
+        "expiry_date",
+        "verified_at",
+        "verified_by",
+        "created_at",
+        "updated_at"
+    ];
+}
