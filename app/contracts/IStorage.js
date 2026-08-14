912e51631b6629d289177310d554b8b87d0bb665
maliq273 <maliq273@gmail.com>
2026-08-14 15:43:05 +0200

    Update IStorage.js

--- app/contracts/IStorage.js
+++ app/contracts/IStorage.js
@@ -1,1 +1,50 @@
+/**
+ * Isaacs and Partners
+ * Contract: Storage
+ */
 
+export class IStorage {
+  initialise() {
+    throw new Error(
+      "IStorage.initialise() must be implemented."
+    );
+  }
+
+  get(key, defaultValue = null) {
+    throw new Error(
+      "IStorage.get() must be implemented."
+    );
+  }
+
+  set(key, value) {
+    throw new Error(
+      "IStorage.set() must be implemented."
+    );
+  }
+
+  remove(key) {
+    throw new Error(
+      "IStorage.remove() must be implemented."
+    );
+  }
+
+  has(key) {
+    throw new Error(
+      "IStorage.has() must be implemented."
+    );
+  }
+
+  clear() {
+    throw new Error(
+      "IStorage.clear() must be implemented."
+    );
+  }
+
+  transaction(callback) {
+    throw new Error(
+      "IStorage.transaction() must be implemented."
+    );
+  }
+}
+
+export default IStorage;
