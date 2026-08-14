ffc649a2ebe8deaca41198db41243a155be100dd
maliq273 <maliq273@gmail.com>
2026-08-14 11:56:30 +0200

    Create index.js

---
+++ app/database/seeds/index.js
@@ -0,0 +1,19 @@
+export {
+    default as CompanySeed
+} from "./CompanySeed.js";
+
+export {
+    default as UserSeed
+} from "./UserSeed.js";
+
+export {
+    default as KnowledgeSeed
+} from "./KnowledgeSeed.js";
+
+export {
+    default as ReferenceDataSeed
+} from "./ReferenceDataSeed.js";
+
+export {
+    default as SeedRunner
+} from "./SeedRunner.js";
