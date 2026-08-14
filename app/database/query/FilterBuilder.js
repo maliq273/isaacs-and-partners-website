ea74f31732b430c90ec98ebd7be0e04b8c569fd5
maliq273 <maliq273@gmail.com>
2026-08-14 11:21:36 +0200

    Update FilterBuilder.js

--- app/database/query/FilterBuilder.js
+++ app/database/query/FilterBuilder.js
@@ -1,1 +1,96 @@
+import {
+    quoteIdentifier
+} from "../functions/DatabaseFunctions.js";
 
+export default class FilterBuilder {
+    constructor() {
+        this.filters = [];
+        this.parameters = {};
+        this.counter = 0;
+    }
+
+    add(
+        column,
+        operator,
+        value
+    ) {
+        const parameter =
+            `filter_${++this.counter}`;
+
+        this.filters.push(
+            `${quoteIdentifier(column)} ${operator} :${parameter}`
+        );
+
+        this.parameters[parameter] =
+            value;
+
+        return this;
+    }
+
+    equals(column, value) {
+        return this.add(
+            column,
+            "=",
+            value
+        );
+    }
+
+    notEquals(column, value) {
+        return this.add(
+            column,
+            "!=",
+            value
+        );
+    }
+
+    greaterThan(column, value) {
+        return this.add(
+            column,
+            ">",
+            value
+        );
+    }
+
+    greaterThanOrEqual(
+        column,
+        value
+    ) {
+        return this.add(
+            column,
+            ">=",
+            value
+        );
+    }
+
+    lessThan(column, value) {
+        return this.add(
+            column,
+            "<",
+            value
+        );
+    }
+
+    lessThanOrEqual(
+        column,
+        value
+    ) {
+        return this.add(
+            column,
+            "<=",
+            value
+        );
+    }
+
+    build() {
+        return {
+            sql: this.filters.length
+                ? this.filters.join(
+                      " AND "
+                  )
+                : "1 = 1",
+            parameters: {
+                ...this.parameters
+            }
+        };
+    }
+}
