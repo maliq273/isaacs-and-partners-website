759f195de55f9ac088fe6ddc94e16fd67f9db1c7
maliq273 <maliq273@gmail.com>
2026-08-14 15:43:37 +0200

    Update IWorkflow.js

--- app/contracts/IWorkflow.js
+++ app/contracts/IWorkflow.js
@@ -1,1 +1,56 @@
+/**
+ * Isaacs and Partners
+ * Contract: Workflow
+ */
 
+export class IWorkflow {
+  initialise(context = {}) {
+    throw new Error(
+      "IWorkflow.initialise() must be implemented."
+    );
+  }
+
+  start(context = {}) {
+    throw new Error(
+      "IWorkflow.start() must be implemented."
+    );
+  }
+
+  execute(input, context = {}) {
+    throw new Error(
+      "IWorkflow.execute() must be implemented."
+    );
+  }
+
+  pause(context = {}) {
+    throw new Error(
+      "IWorkflow.pause() must be implemented."
+    );
+  }
+
+  resume(context = {}) {
+    throw new Error(
+      "IWorkflow.resume() must be implemented."
+    );
+  }
+
+  cancel(context = {}) {
+    throw new Error(
+      "IWorkflow.cancel() must be implemented."
+    );
+  }
+
+  complete(result = null, context = {}) {
+    throw new Error(
+      "IWorkflow.complete() must be implemented."
+    );
+  }
+
+  getStatus() {
+    throw new Error(
+      "IWorkflow.getStatus() must be implemented."
+    );
+  }
+}
+
+export default IWorkflow;
