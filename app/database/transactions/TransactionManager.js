0b6cb76ec44d2da52f7388d5e99cb590b1428d77
maliq273 <maliq273@gmail.com>
2026-08-14 11:24:46 +0200

    Update TransactionManager.js

--- app/database/transactions/TransactionManager.js
+++ app/database/transactions/TransactionManager.js
@@ -1,1 +1,82 @@
+import Transaction from "./Transaction.js";
 
+export default class TransactionManager {
+    constructor(database) {
+        this.database = database;
+        this.currentTransaction =
+            null;
+    }
+
+    begin() {
+        if (this.currentTransaction) {
+            throw new Error(
+                "A transaction is already active"
+            );
+        }
+
+        this.currentTransaction =
+            new Transaction(
+                this.database
+            );
+
+        this.currentTransaction.begin();
+
+        return this.currentTransaction;
+    }
+
+    commit() {
+        if (!this.currentTransaction) {
+            throw new Error(
+                "No active transaction"
+            );
+        }
+
+        const transaction =
+            this.currentTransaction;
+
+        try {
+            transaction.commit();
+        } finally {
+            this.currentTransaction =
+                null;
+        }
+    }
+
+    rollback() {
+        if (!this.currentTransaction) {
+            return;
+        }
+
+        try {
+            this.currentTransaction.rollback();
+        } finally {
+            this.currentTransaction =
+                null;
+        }
+    }
+
+    async run(callback) {
+        const transaction =
+            this.begin();
+
+        try {
+            const result =
+                await callback(
+                    transaction
+                );
+
+            this.commit();
+
+            return result;
+        } catch (error) {
+            this.rollback();
+            throw error;
+        }
+    }
+
+    isActive() {
+        return Boolean(
+            this.currentTransaction
+        );
+    }
+}
