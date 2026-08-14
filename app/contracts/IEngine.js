c07cd83411755300de39b96bd2a6497129158635
maliq273 <maliq273@gmail.com>
2026-08-14 15:39:44 +0200

    Update IEngine.js

--- app/contracts/IEngine.js
+++ app/contracts/IEngine.js
@@ -1,1 +1,32 @@
+/**
+ * Isaacs and Partners
+ * Contract: Engine
+ */
 
+export class IEngine {
+  initialise(context = {}) {
+    throw new Error(
+      "IEngine.initialise() must be implemented."
+    );
+  }
+
+  execute(input, context = {}) {
+    throw new Error(
+      "IEngine.execute() must be implemented."
+    );
+  }
+
+  validate(input, context = {}) {
+    throw new Error(
+      "IEngine.validate() must be implemented."
+    );
+  }
+
+  reset() {
+    throw new Error(
+      "IEngine.reset() must be implemented."
+    );
+  }
+}
+
+export default IEngine;
