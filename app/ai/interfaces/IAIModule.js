510cd5f0d3f0ead86737f934f7d0be85f28ca89a
maliq273 <maliq273@gmail.com>
2026-08-15 11:33:20 +0200

    Update IAIModule.js

--- app/ai/interfaces/IAIModule.js
+++ app/ai/interfaces/IAIModule.js
@@ -1,1 +1,32 @@
+export default class IAIModule {
+    constructor(name) {
+        if (!name) {
+            throw new Error("AI module name is required");
+        }
 
+        this.name = name;
+        this.enabled = true;
+    }
+
+    async initialize() {
+        return true;
+    }
+
+    async execute() {
+        throw new Error(
+            `${this.name} must implement execute()`
+        );
+    }
+
+    async shutdown() {
+        return true;
+    }
+
+    health() {
+        return {
+            name: this.name,
+            enabled: this.enabled,
+            healthy: true
+        };
+    }
+}
