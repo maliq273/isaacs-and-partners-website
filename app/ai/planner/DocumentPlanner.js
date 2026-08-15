fb098c7d6832735c03d1bef0a3e6926d53714c7f
maliq273 <maliq273@gmail.com>
2026-08-15 12:15:11 +0200

    Update DocumentPlanner.js

--- app/ai/planner/DocumentPlanner.js
+++ app/ai/planner/DocumentPlanner.js
@@ -1,1 +1,31 @@
+export default class DocumentPlanner {
+    plan({
+        required = [],
+        supplied = []
+    } = {}) {
+        const suppliedTypes =
+            new Set(
+                supplied.map(
+                    document =>
+                        document.type
+                )
+            );
 
+        return required
+            .filter(
+                requirement =>
+                    !suppliedTypes.has(
+                        requirement.type ||
+                        requirement
+                    )
+            )
+            .map(
+                requirement => ({
+                    action:
+                        "REQUEST_DOCUMENT",
+                    document:
+                        requirement
+                })
+            );
+    }
+}
