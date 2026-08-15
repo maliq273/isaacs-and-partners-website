82699614eebd11b4a2d09113ca7be1bd17f47d56
maliq273 <maliq273@gmail.com>
2026-08-15 12:15:33 +0200

    Update QuestionPlanner.js

--- app/ai/planner/QuestionPlanner.js
+++ app/ai/planner/QuestionPlanner.js
@@ -1,1 +1,13 @@
-
+export default class QuestionPlanner {
+    plan({
+        questions = [],
+        answers = {}
+    } = {}) {
+        return questions.filter(
+            question =>
+                answers[
+                    question.id
+                ] === undefined
+        );
+    }
+}
