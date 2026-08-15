3c3b724e305a2cfcee786e26b10843eacd509541
maliq273 <maliq273@gmail.com>
2026-08-15 11:44:15 +0200

    Update DebugConsole.js

--- app/ai/diagnostics/DebugConsole.js
+++ app/ai/diagnostics/DebugConsole.js
@@ -1,1 +1,27 @@
+export default class DebugConsole {
+    constructor({
+        enabled = false,
+        logger = console
+    } = {}) {
+        this.enabled = enabled;
+        this.logger = logger;
+    }
 
+    log(...args) {
+        if (this.enabled) {
+            this.logger.log(...args);
+        }
+    }
+
+    warn(...args) {
+        if (this.enabled) {
+            this.logger.warn(...args);
+        }
+    }
+
+    error(...args) {
+        if (this.enabled) {
+            this.logger.error(...args);
+        }
+    }
+}
