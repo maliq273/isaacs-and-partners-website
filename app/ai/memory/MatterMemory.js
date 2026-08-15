0e2e363cef93a51f4c6e6aac5659cea52fc44174
maliq273 <maliq273@gmail.com>
2026-08-15 12:09:13 +0200

    Update MatterMemory.js

--- app/ai/memory/MatterMemory.js
+++ app/ai/memory/MatterMemory.js
@@ -1,1 +1,26 @@
+export default class MatterMemory {
+    constructor() {
+        this.matters = new Map();
+    }
 
+    set(matterId, data) {
+        this.matters.set(
+            matterId,
+            {
+                ...data
+            }
+        );
+    }
+
+    get(matterId) {
+        return this.matters.get(
+            matterId
+        );
+    }
+
+    clear(matterId) {
+        this.matters.delete(
+            matterId
+        );
+    }
+}
