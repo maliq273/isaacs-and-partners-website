60a0a2ff08ec63e78a099d84ef2f683b9c725b71
maliq273 <maliq273@gmail.com>
2026-08-15 12:14:12 +0200

    Update WorkflowRuntime.js

--- app/ai/runtime/WorkflowRuntime.js
+++ app/ai/runtime/WorkflowRuntime.js
@@ -1,1 +1,39 @@
+export default class WorkflowRuntime {
+    constructor({
+        stepExecutor,
+        checkpointManager
+    }) {
+        this.stepExecutor =
+            stepExecutor;
 
+        this.checkpointManager =
+            checkpointManager;
+    }
+
+    async run(
+        workflow,
+        context
+    ) {
+        for (
+            let index = 0;
+            index <
+            workflow.steps.length;
+            index++
+        ) {
+            this.checkpointManager?.save(
+                context.id,
+                {
+                    step: index,
+                    context
+                }
+            );
+
+            await this.stepExecutor.execute(
+                workflow.steps[index],
+                context
+            );
+        }
+
+        return context;
+    }
+}
