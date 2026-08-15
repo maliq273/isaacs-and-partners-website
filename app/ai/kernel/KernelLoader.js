8bd4b97fd576bea6c181592e818140c806671745
maliq273 <maliq273@gmail.com>
2026-08-15 11:39:53 +0200

    Add KernelLoader class for loading modules

--- app/ai/kernel/KernelLoader.js
+++ app/ai/kernel/KernelLoader.js
@@ -1,1 +1,24 @@
+export default class KernelLoader {
+    constructor({
+        container,
+        registry,
+        moduleLoader
+    }) {
+        this.container = container;
+        this.registry = registry;
+        this.moduleLoader =
+            moduleLoader;
+    }
 
+    async load(modules = []) {
+        for (const definition of modules) {
+            await this.moduleLoader.load(
+                definition.name,
+                definition.module,
+                definition.options || {}
+            );
+        }
+
+        return this.registry;
+    }
+}
