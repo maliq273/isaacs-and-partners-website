a974e46bdaa42d29453e93f24a02050af0ba78a9
maliq273 <maliq273@gmail.com>
2026-08-15 11:35:44 +0200

    Update ISkill.js

--- app/ai/interfaces/ISkill.js
+++ app/ai/interfaces/ISkill.js
@@ -1,1 +1,15 @@
+export default class ISkill {
+    constructor(name) {
+        this.name = name;
+    }
 
+    supports() {
+        return false;
+    }
+
+    async execute() {
+        throw new Error(
+            `${this.name} must implement execute()`
+        );
+    }
+}
