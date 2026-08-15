cdbcecc880357c5c03ebafd3deb1ad96c1b5006f
maliq273 <maliq273@gmail.com>
2026-08-15 12:06:41 +0200

    Update DecisionTree.js

--- app/ai/decision/DecisionTree.js
+++ app/ai/decision/DecisionTree.js
@@ -1,1 +1,14 @@
+export default class DecisionTree {
+    constructor(root = null) {
+        this.root = root;
+    }
 
+    setRoot(node) {
+        this.root = node;
+        return this;
+    }
+
+    getRoot() {
+        return this.root;
+    }
+}
