aa5184adbc46fc6cf4e5e7e7394d3ee8dd36c2f9
maliq273 <maliq273@gmail.com>
2026-08-15 12:10:35 +0200

    Update RuleRegistry.js

--- app/ai/rules/RuleRegistry.js
+++ app/ai/rules/RuleRegistry.js
@@ -1,1 +1,21 @@
+export default class RuleRegistry {
+    constructor() {
+        this.rules = new Map();
+    }
 
+    register(name, rule) {
+        this.rules.set(name, rule);
+    }
+
+    get(name) {
+        return this.rules.get(name);
+    }
+
+    has(name) {
+        return this.rules.has(name);
+    }
+
+    list() {
+        return [...this.rules.keys()];
+    }
+}
