9f3ef4b91aea2391c01def77de1e081c6f596e63
maliq273 <maliq273@gmail.com>
2026-08-15 12:06:19 +0200

    Update DecisionNode.js

--- app/ai/decision/DecisionNode.js
+++ app/ai/decision/DecisionNode.js
@@ -1,1 +1,17 @@
-
+export default class DecisionNode {
+    constructor({
+        id,
+        question = null,
+        condition = null,
+        trueNode = null,
+        falseNode = null,
+        result = null
+    } = {}) {
+        this.id = id;
+        this.question = question;
+        this.condition = condition;
+        this.trueNode = trueNode;
+        this.falseNode = falseNode;
+        this.result = result;
+    }
+}
