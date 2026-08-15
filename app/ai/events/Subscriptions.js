ff5be56387c0655fbefc0c22764b4d4e6c2b582d
maliq273 <maliq273@gmail.com>
2026-08-15 11:41:29 +0200

    Create Subscriptions class for event handling
    Added Subscriptions class to manage event subscriptions.

--- app/ai/events/Subscriptions.js
+++ app/ai/events/Subscriptions.js
@@ -1,1 +1,26 @@
+export default class Subscriptions {
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+        this.cleanups = [];
+    }
 
+    on(event, handler) {
+        const cleanup =
+            this.eventBus.subscribe(
+                event,
+                handler
+            );
+
+        this.cleanups.push(cleanup);
+
+        return cleanup;
+    }
+
+    clear() {
+        for (const cleanup of this.cleanups) {
+            cleanup();
+        }
+
+        this.cleanups = [];
+    }
+}
