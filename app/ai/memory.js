6ff013089d4ff7a76d87e8195a6fba886ecfc792
maliq273 <maliq273@gmail.com>
2026-08-15 12:27:53 +0200

    Implement AIMemory class with memory management
    Added AIMemory class to manage different memory types.

--- app/ai/memory.js
+++ app/ai/memory.js
@@ -1,1 +1,27 @@
+import SessionMemory from "./memory/SessionMemory.js";
+import ConversationMemory from "./memory/ConversationMemory.js";
+import MatterMemory from "./memory/MatterMemory.js";
+import KnowledgeMemory from "./memory/KnowledgeMemory.js";
 
+export default class AIMemory {
+    constructor() {
+        this.session =
+            new SessionMemory();
+
+        this.conversation =
+            new ConversationMemory();
+
+        this.matter =
+            new MatterMemory();
+
+        this.knowledge =
+            new KnowledgeMemory();
+    }
+
+    clear() {
+        this.session.clear();
+        this.conversation.clear();
+        this.matter.matters.clear();
+        this.knowledge.clear();
+    }
+}
