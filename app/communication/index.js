d44367709f6af385551c379e3eb8689737bb5256
maliq273 <maliq273@gmail.com>
2026-09-03 14:09:28 +0200

    PR47 — Export WhatsApp communication domain

---
+++ app/communication/index.js
@@ -0,0 +1,8 @@
+export { default as WhatsAppAgent } from "./agents/WhatsAppAgent.js";
+export { default as WhatsAppIntentClassifier } from "./classifiers/WhatsAppIntentClassifier.js";
+export { default as ConversationService, CONVERSATION_STATES } from "./services/ConversationService.js";
+export { default as HandoverService } from "./services/HandoverService.js";
+export { default as LeadService } from "./services/LeadService.js";
+export { default as SalesService } from "./services/SalesService.js";
+export { default as StaffMessageHandler } from "./handlers/StaffMessageHandler.js";
+export { default as EscalationHandler } from "./handlers/EscalationHandler.js";
